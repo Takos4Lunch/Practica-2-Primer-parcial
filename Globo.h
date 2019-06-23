17fa863abbd53a68240231256bc6d2d0e3b9a4c3
Rafael <takeshie01@gmail.com>
2019-06-22 22:29:45 -0430

    New class added & modified main

---
+++ Globo.h
@@ -0,0 +1,20 @@
+#ifndef GloboH
+#define GloboH
+
+#include "Consumo.h"
+
+class Globo{
+	char codigo[10];
+	char propietario[25];
+	int cantidadMediciones;
+	Consumo **consumos;
+	public:
+		Globo();//Constructor
+		Globo(char * cod, char* prop, int cant, Consumo ** cnsm);//Parametric Constructor
+		char getCodigo();
+		char getPropietario();
+		int getMediciones();
+		Consumo ** getConsumos();
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
-
+#include "Data.h"
+#include "Consumo.h"
+#include "Freon12.h"
+#include "Helio.h"
+#include "Globo.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+class Principal{
+	Globo * vGlobos;
+};
 
 int main(int argc, char** argv) {
 	return 0;
 }
+
+

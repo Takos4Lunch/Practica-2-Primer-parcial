fe32448d6c1c69606d1369be39247e444699efee
Rafael <takeshie01@gmail.com>
2019-06-23 19:51:53 -0430

    Final version

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <typeinfo>
+#include <iomanip>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -74,7 +75,9 @@ class Principal{
 		void mostrarDatos(){
 			char ayudante1[50], ayudante2[50];
 			int ayudante3;
-			int fr, hel, cantfr, canthel, cantrang;
+			int helpfr, cantfr, canthel, cantrang;
+			float hel, fr, globalhel=0, globalfr=0;
+			int contfr, conthel;
 			Consumo **cnsm;
 			
 			for(int x =0;x<data.getLongitud();x++){
@@ -88,22 +91,41 @@ class Principal{
 				fr = 0;
 				hel = 0;
 				cantrang = 0;
+				helpfr=0;
 				cnsm = new Consumo*[ayudante3];
 				cnsm = vGlobos[x]->getConsumos();
 				for(int y=0;y<ayudante3;y++){
 					if(typeid(*cnsm[y])==typeid(Freon12)){
-						fr = fr + cnsm[y]->getCantidad();
+						fr = fr + 0.350;
+						globalfr = globalfr + 0.350;
+						contfr++;
 					}else if(typeid(*cnsm[y])==typeid(Helio)){
-						hel = hel + cnsm[y]->getCantidad();
+						hel = hel + 0.5;
+						globalhel = globalhel + 0.5;
+						conthel++;
 					}else{
 						cantrang++;
 					}
 				}
-				
 				cout<<"Helio usado: "<<hel<<" litros"<<endl;
-				cout<<"Freon12 usado: "<<fr<<" gramos"<<endl;
+				cout<<"Freon12 usado: "<<fr<<" kilogramos"<<endl;
 				cout<<cantrang<<" veces se mantuvo dentro del rango de seguridad"<<endl;
 				cout<<""<<endl;
+				if(fr>=5){
+					cout<<"Se han usado mas de 5 lts de Helio"<<endl;
+				}
+				if(hel>=5){
+					cout<<"Se han usado mas de 5 kgs de Freon12"<<endl;
+				}
+			}
+			
+			if(contfr>=7){
+				cout<<"En este viaje, se usaron "<<globalfr<<" Kilogramos de Freon en total."<<endl<<"Para el proximo viaje, se debe llevar el doble de gas Freon12, exactamente "<<globalfr*2<<" kilogramos"<<endl;
+				cout<<""<<endl;
+			}
+			
+			if(conthel>=3){
+				cout<<"En este viaje, se usaron "<<globalhel<<" Kilogramos de Freon en total."<<endl<<"Para el proximo viaje, se debe llevar el doble de gas Helio, exactamente "<<globalhel*2<<" litros"<<endl;
 			}
 		}
 };
@@ -112,6 +134,7 @@ int main(int argc, char** argv) {
 	Principal x;
 	x.cargarDatos();
 	x.mostrarDatos();
+	//Realizado por Rafael Ramírez y Luis Acosta
 	return 0;
 }
 
